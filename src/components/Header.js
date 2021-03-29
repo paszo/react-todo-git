adb980e993e9ea652365fed28dc0c4f9136585d8
paszo <paszo2008@gmail.com>
2021-03-29 18:26:36 +0200

    Added button

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,10 @@
+import Button from "./Button";
+
 const Header = () => {
     return (
         <header className="header">
             <h1>Header</h1>
+            <Button/>
         </header>
     )
 }
